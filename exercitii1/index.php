399ca381a70d05427e93b797cdb8b7553b998c1f
Ecaterina <barganecaterina@yahoo.com>
2023-05-23 21:44:09 +0300

    first commit

---
+++ exercitii1/index.php
@@ -0,0 +1,16 @@
+<?php
+
+//expresia matematica: //nrbinar(de la dreapta la stanga) * 2**exponent(de la 0 la max)
+
+// $binar = "110011110";
+// $nrDec = 0;
+// $exp = 0;
+
+// for($i=strlen($binar)-1; $i>=0; $i--) {
+//   $nrDec += intval($binar[$i]) * (2 ** $exp);
+//   $exp++;
+// }
+
+// echo $nrDec;
+
+?>
