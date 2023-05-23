dcd156c20e5a61a00ebc9397a9c8e26449d1b245
Ecaterina <barganecaterina@yahoo.com>
2023-05-23 23:57:44 +0300

    string/index: create

---
+++ string/index.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$string = "        Salut       ";
+// $string[2] = "r";
+// echo $string;
+
+//$faraSp = trim($string);
+
+echo $string . "<br>" ;
+//. $faraSp;
+
+$lorem = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus reiciendis rerum perspiciatis nam obcaecati, inventore nulla at! Non culpa alias quidem adipisci sed, quos tempore eveniet deserunt vitae suscipit veniam possimus minus mollitia maiores consequatur. Ratione ad voluptate sunt non illum temporibus, commodi pariatur praesentium deserunt autem dignissimos nemo illo";
+
+$nr_cuv = str_word_count($lorem);
+echo $nr_cuv;
+
+
+
+
+?>
