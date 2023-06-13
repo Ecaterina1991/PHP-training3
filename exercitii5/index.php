64d3544e31e51c3f17c6eecfd7508195ca322788
Ecaterina <barganecaterina@yahoo.com>
2023-06-13 23:00:39 +0300

    exercitii5/index:

--- exercitii5/index.php
+++ exercitii5/index.php
@@ -84,16 +84,49 @@
 
 
 
+// Exercițiul 1: Suma cifrelor
+// Scrie un program în PHP care calculează suma cifrelor unui număr dat de la tastatură.
+
+
+// $nr = "856942";
+// $sum=0;
+
+// for($i=0; $i<=strlen($nr)-1; $i++) {
+// $sum+=$nr[$i];
+// }
+
+// echo $sum;
+
+// Tabel de înmulțire
+// Scrie un program în PHP care afișează tabelul de înmulțire de la 1 la 10.
+
+// for ($i=1; $i<=10; $i++) {
+// echo "1"."*". $i ."="."$i". "<br>";
+// }
+
+// echo "<table>";
+// for ($i = 1; $i <= 10; $i++) {
+//     echo "<tr>";
+//     for ($j = 1; $j <= 10; $j++) {
+//         echo "<td>" . ($i * $j) . "</td>";
+//     }
+//     echo "</tr>";
+// }
+// echo "</table>";
+
+//bucla externa determina repetarea de 10 ori a randului tr in interiorul tabelului
+//in fiecare rand se afiseaza 10 celule datorita buclei interne j si in fiecare celula td se afiseaza rezultatul i*j
 
 
 
 
 
 
+// Exercițiul 3: Fibonacci
+// Scrie un program în PHP care afișează primele n numere din seria Fibonacci, unde n este un număr dat de la tastatură.
 
 
-
-
+//Numerele Fibonacci sunt numere naturale care fac parte din următorul șir, în care fiecare număr este egal cu suma celor două de dinainte: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, …
 
 
 
